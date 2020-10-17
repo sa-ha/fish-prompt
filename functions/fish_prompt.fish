85e38b4aee6ffc3371854bd61b0f0130ce822688
sa-ha <sa.ha.1339@gmail.com>
2020-10-18 07:18:09 +0900

    プロンプトにgitを追加

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,32 +1,30 @@
 # by SHA-320
 # prompt requires: git
 
-# global settings
-set -g __fish_git_prompt_showcolorhints 'yep'
-set -g __fish_git_prompt_showdirtystate 'yep'
-set -g __fish_git_prompt_showupstream 'auto'
-set -g __fish_git_prompt_show_informative_status 'why not'
+# Options
+set __fish_git_prompt_showcolorhints 'yes'
+#set __fish_git_prompt_showuntrackedfiles "informative"
+set __fish_git_prompt_char_dirtystate '+'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
 
 function fish_prompt
     set -l last_status $status
 
     set -l right_prompt (set_color magenta)'['(date +%H:%M)']' (set_color cyan)(prompt_pwd)
+    set_color normal
 
    	if git_is_repo
-    	set -l stashed ''
-
-    	if git_is_stashed
-      		set stashed '(stashed)'
-    	end
-
-        if test $last_status -ne 0
-            set pcolor $red
-        end
-
-    	set right_prompt $right_prompt (set_color green --bold)'('(git_branch_name)$stashed')'
+        # git user name
+        #set git_user (git config --get user.name) 2> /dev/null
+        # git current repository
+        set git_repo basename -s .git (git config --get remote.origin.url) 2> /dev/null
+    	set right_prompt $right_prompt (set_color normal)'['(set_color blue)(git config --get user.name 2> /dev/null)/($git_repo)(set_color normal)']'
+        # offical git prompt
+        set right_prompt $right_prompt (fish_git_prompt)
   	end
 
-    
     # line 1
   	echo $right_prompt
 
@@ -36,7 +34,12 @@ function fish_prompt
     test $USER = 'root'
         and echo (set_color red)"#"
 
+
     # line 2 (Main)
     echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
     set_color normal
+end
+
+function fish_right_prompt
+    
 end
